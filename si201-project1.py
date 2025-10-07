602b8d818479da9cc08ad4f33ba2265d272c5c2d
Levi barense <barense@umich.edu>
2025-10-06 21:34:15 -0400

    TItled and started calculations

---
+++ si201-project1.py
@@ -0,0 +1,7 @@
+# Name: Levi Barense
+# Dataset: Sample Superstore Dataset
+# Calculation #1: Calculate the average revenue per region given
+# Calculation #2: Calculate the average revenue per given product category
+# Column #1: Category
+# Column #2: Sales
+# Column #3: Region
