74ad1eccba10691da23350824efc77fe94bbbe50
Levi barense <barense@umich.edu>
2025-10-15 20:27:48 -0400

    revPerRegion and revPerCategory

--- si201-project1.py
+++ si201-project1.py
@@ -5,3 +5,48 @@
 # Column #1: Category
 # Column #2: Sales
 # Column #3: Region
+
+
+
+import os
+
+
+
+def starter():
+    import csv
+    data = []
+    folder = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(folder, "SampleSuperstore.csv")
+    with open(file_path, newline = '') as f:
+        reader = csv.reader(f)
+        for x in reader:
+            data.append(x)
+        
+    return data
+
+
+def revPerRegion(region, data):
+    # print (data)
+    totalRev = 0
+    quant = 0
+    for x in data:
+        if x[6] == region:
+            totalRev += float(x[-1])
+            quant += 1
+            # print (totalRev)
+    return totalRev / quant
+
+def revPerCategory(category, data):
+    totalRev = 0
+    quant = 0
+    for x in data:
+        if x[7] == category:
+            totalRev += float(x[-1])
+            quant += 1
+    return totalRev / quant
+
+            
+data = starter()
+print(revPerRegion("East", data))
+print(revPerCategory("Technology", data))
+
