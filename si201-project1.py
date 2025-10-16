9655553cf4b02f81a90e818404c4ea5be1cf167a
Levi barense <barense@umich.edu>
2025-10-16 00:43:27 -0400

    idek man

--- si201-project1.py
+++ si201-project1.py
@@ -26,25 +26,30 @@ def starter():
         
     return data
 
-def retrieveRegion(data):
+def retrieveRegion(datawithheader):
+    data = datawithheader[1:]
     regions = []
     for x in data:
         if x[6] not in regions:
             regions.append(x[6])
     print (regions)
     regionSel = input(f"Select a Region")
+    # account for typos and test
     print (regionSel)
     dict = {}
+    reg = []
     for x in range(len(data)):
         # print (x)
         if (data[x][6]) == regionSel:
-            dict[x] = [regionSel, (data[x][3]), (data[x][4]), (data[x][5]), (data[x][7]), f"Product: {data[x][8]}", (data[x][9])]
-    # max_key = max(dict.keys())
-    # print("max key:" , max_key)
+            reg.append(x)
+    # print (reg)
+    for x in range(len(reg)):
+        dict[x] = [regionSel, (data[x][3]), (data[x][4]), (data[x][5]), (data[x][7]), (data[x][8]), (data[x][9])]
+
     return dict
             
 
-
+['East', 'City', 'State', 'Postal Code', 'Category', 'Sub-Category', 'Sales']
         
 
 def revPerRegion(data):
@@ -52,7 +57,7 @@ def revPerRegion(data):
     totalRev = 0
     quant = 0
     for x in data.values():
-        totalRev += float(x[-1])
+        totalRev += float(x[6])
         quant += 1
         # print (totalRev)
     return totalRev / quant
@@ -61,19 +66,36 @@ def revPerCategory(category, data):
     totalRev = 0
     quant = 0
     for x in data.values():
-            totalRev += float(x[-1])
+            totalRev += float(x[6])
             quant += 1
     return totalRev / quant
 
+def retrieveCategory(data):
+    cats = []
+    for x in data.values():
+        if x[5] not in cats:
+            cats.append(x[5])
+    print (cats)
+    catSelect = input(f"Select a category")
+    return catSelect
 
 raw = starter()
 regionD = retrieveRegion(raw)
 avgRegionRev = revPerRegion(regionD)
-
+print (avgRegionRev)
+catSelect = retrieveCategory(regionD)
+avgCatRev = revPerCategory(catSelect, regionD)
+print (avgCatRev)
+max_key = len(regionD.keys())
+num = input(f"pick a number between 1 and {max_key}:")
+print(regionD[num])
  
 
-# ['West', 'Anaheim', 'California', '92804', 'West', 'Office Supplies']
-
+# ['South', 'Miami', 'Florida', '33180', 'Furniture', 'Product: Furnishings', '25.248']
+"""
+9981: ['South', 'Lafayette', 'Louisiana', '70506', 'Furniture', {'Tables'}, '85.98'], 
+9988: ['South', 'Athens', 'Georgia', '30605', 'Technology', {'Accessories'}, '79.99']
+"""
 # def main(regSelection):                
 #     raw = starter()
 #     print 
