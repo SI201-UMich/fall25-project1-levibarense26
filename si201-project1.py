17f113a1ae8cf88e2525258ecb90a39ae9382892
Levi barense <barense@umich.edu>
2025-10-16 01:54:27 -0400

    compiled conclusion function

--- si201-project1.py
+++ si201-project1.py
@@ -7,7 +7,7 @@
 # Column #3: Region
 
 
-
+import random
 import os
 
     
@@ -42,9 +42,9 @@ def retrieveRegion(datawithheader):
         # print (x)
         if (data[x][6]) == regionSel:
             reg.append(x)
-    # print (reg)
+    print (f" There are {(len(reg))} rows in this region")
     for x in range(len(reg)):
-        dict[x] = [regionSel, (data[x][3]), (data[x][4]), (data[x][5]), (data[x][7]), (data[x][8]), (data[x][9])]
+        dict[x] = [regionSel, (data[x][3]), (data[x][4]), (data[x][5]), (data[x][7]), (data[x][8]), (data[x][9]), (data[x][12]), (data[x][10])]
 
     return dict
             
@@ -66,6 +66,7 @@ def revPerCategory(category, data):
     totalRev = 0
     quant = 0
     for x in data.values():
+        if x[4] == category:
             totalRev += float(x[6])
             quant += 1
     return totalRev / quant
@@ -73,35 +74,37 @@ def revPerCategory(category, data):
 def retrieveCategory(data):
     cats = []
     for x in data.values():
-        if x[5] not in cats:
-            cats.append(x[5])
+        if x[4] not in cats:
+            cats.append(x[4])
     print (cats)
     catSelect = input(f"Select a category")
     return catSelect
 
-raw = starter()
-regionD = retrieveRegion(raw)
-avgRegionRev = revPerRegion(regionD)
-print (avgRegionRev)
-catSelect = retrieveCategory(regionD)
-avgCatRev = revPerCategory(catSelect, regionD)
-print (avgCatRev)
-max_key = len(regionD.keys())
-num = input(f"pick a number between 1 and {max_key}:")
-print(regionD[num])
- 
+def conclusion():
+    raw = starter()
+    regionD = retrieveRegion(raw)
+    avgRegionRev = revPerRegion(regionD)
+    avgRegion = (f" The average revenue for the region is {avgRegionRev}")
+    print (avgRegion)
+    catSelect = retrieveCategory(regionD)
+    avgCatRev = revPerCategory(catSelect, regionD)
+    avgCat = (f" The average revenye of the {catSelect} category is {avgCatRev}")
+    print (avgCat)
+    # for x in regionD.keys():
+    #     print (x) 
+    keysList = list(regionD.keys())
+    randKeys = random.sample(keysList, 5) 
+    for x in randKeys:
+        print(x, regionD[x][0])
+    num = input(f"pick a number:")
+    # if num isnt a int
+    num = int(num)
+    productInfo = (f"Your selected product is in the {regionD[num][4]}, and are {regionD[num][5]}, with {regionD[num][6]} dollars of revenue. They've earned a profit of {regionD[num][7]} dollars by selling {regionD[num][8]} units.")
+    print (productInfo)
+
 
 # ['South', 'Miami', 'Florida', '33180', 'Furniture', 'Product: Furnishings', '25.248']
 """
 9981: ['South', 'Lafayette', 'Louisiana', '70506', 'Furniture', {'Tables'}, '85.98'], 
 9988: ['South', 'Athens', 'Georgia', '30605', 'Technology', {'Accessories'}, '79.99']
 """
-# def main(regSelection):                
-#     raw = starter()
-#     print 
-#     regionD = (retrieveRegion(raw))
-#     revPerRegion(regSelection, regionD)
-    #
-    # print(revPerCategory(, data))
-#
-# main(regionSelection 
